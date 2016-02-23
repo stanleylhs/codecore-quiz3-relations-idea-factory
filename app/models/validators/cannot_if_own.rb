d6f8ab37ede99a7c1e866f1d3e2da576172e0ca3
Stanley Hansen <stanley321@gmail.com>
2016-02-22 19:20:41 -0800

    Typo

--- app/models/validators/cannot_if_own.rb
+++ app/models/validators/cannot_if_own.rb
@@ -2,8 +2,7 @@ class Validators::CannotIfOwn < ActiveModel::Validator
 
   def validate(record)
     unless record.user != record.idea.user
-      record.errors[:user_id] << "You can't perform this task on your own idea"
-      #{record.class.name}!"
+      record.errors[:user_id] << "You can't perform this task on your own idea to #{record.class.name}!"
       # record.idea.errors[:user_id] << "You can't like your own idea!"
     end
   end
