7ea4383e71fd45322c31d970220c0b0511128c85
Stanley Hansen <stanley321@gmail.com>
2016-02-22 19:14:49 -0800

    Refactor Validators.

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,21 +9,11 @@
 #  updated_at :datetime         not null
 #
 
-# TODO: refactor 
-class CannotLikeOwn < ActiveModel::Validator
-  def validate(record)
-    unless record.user != record.idea.user
-      record.errors[:user_id] << "You can't like your own idea!"
-      # record.idea.errors[:user_id] << "You can't like your own idea!"
-    end
-  end
-end
-
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :idea
 
   validates :user_id, uniqueness: {scope: :idea_id}
-  validates_with CannotLikeOwn
+  validates_with Validators::CannotIfOwn
 end
 
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -9,19 +9,10 @@
 #  updated_at :datetime         not null
 #
 
-class CannotLikeOwn < ActiveModel::Validator
-  def validate(record)
-    unless record.user != record.idea.user
-      record.errors[:user_id] << "You can't join your own idea!"
-      # record.idea.errors[:user_id] << "You can't like your own idea!"
-    end
-  end
-end
-
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :idea
 
   validates :user_id, uniqueness: {scope: :idea_id}
-  validates_with CannotLikeOwn
+  validates_with Validators::CannotIfOwn
 end
